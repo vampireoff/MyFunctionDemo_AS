1b82cbee30ca3d0cf9b58ac62232e77cd1b2490a
lwd <452517420@qq.com>
2016-08-17 18:18:17 +0800

    增加jni功能

--- app/src/main/java/com/function/jni/JniUtils.java
+++ app/src/main/java/com/function/jni/JniUtils.java
@@ -1,12 +1,22 @@
 package com.function.jni;
 
+import android.util.Log;
+
 /**
  * Created by lwd on 2016/8/8.
  */
 public class JniUtils {
+    public static int var = 0;
+
     static {
         System.loadLibrary("JniTest");
     }
 
+    public static void myLog() {
+        Log.i("javalog", "jni调用java函数");
+    }
+
     public static native String getStringFromC();
+    public static native String getStringFromC2();
+    public static native void setVar();
 }
